510ebd91088e70729fc5036763547ae13938237d
matbu <mat.bultel@gmail.com>
2024-02-20 11:10:26 +0100

    Allow to populate config.yaml automatically for podman informations

--- cmd/pull.go
+++ cmd/pull.go
@@ -27,8 +27,10 @@ import (
 
 var cloud string
 var output_dir string
-var verbose bool
+var update bool
+var updateOnly bool
 var serviceConfig string
+var filters []string
 
 var pullCmd = &cobra.Command{
 	Use:   "pull",
@@ -37,7 +39,13 @@ var pullCmd = &cobra.Command{
 environment or OCP. For example:
 ./os-diff pull --env=tripleo
 You can set configuration in your os-diff.cfg or provide output directory via the command line:
-./os-diff pull -e ocp -o /tmp/myconfigdir -s my-service-config-file`,
+./os-diff pull -e ocp -o /tmp/myconfigdir -s my-service-config-file
+You can also update the config.yaml file with the information from your TripleO environment:
+./os-diff pull --update-only
+This command will add the podman and image IDs in the config.yaml or also:
+./os-pull pull --update
+This command will populate the config.yaml file with the podman and image Ids and pull the config too.
+`,
 	Run: func(cmd *cobra.Command, args []string) {
 
 		// Get config:
@@ -64,14 +72,20 @@ You can set configuration in your os-diff.cfg or provide output directory via th
 			standaloneSsh := config.Tripleo.SshCmd
 			remoteConfigDir := config.Tripleo.RemoteConfigPath
 			localConfigDir := config.Tripleo.LocalConfigPath
+
 			if !common.TestSshConnection(standaloneSsh) {
 				fmt.Println("Please check your SSH configuration: " + standaloneSsh)
 				return
 			}
-			err := collectcfg.FetchConfigFromEnv(serviceConfig, localConfigDir, remoteConfigDir, true, config.Tripleo.Connection, standaloneSsh)
-			if err != nil {
-				fmt.Println("Error while collecting config: ", err)
-				return
+			if update || updateOnly {
+				collectcfg.SetTripleODataEnv(serviceConfig, standaloneSsh, filters, true)
+			}
+			if !updateOnly {
+				err := collectcfg.FetchConfigFromEnv(serviceConfig, localConfigDir, remoteConfigDir, true, config.Tripleo.Connection, standaloneSsh)
+				if err != nil {
+					fmt.Println("Error while collecting config: ", err)
+					return
+				}
 			}
 		} else {
 			fmt.Println("Error unkown cloud", cloud)
@@ -84,6 +98,8 @@ func init() {
 	pullCmd.Flags().StringVarP(&cloud, "env", "e", "tripleo", "Service engine, could be: ocp or tripleo.")
 	pullCmd.Flags().StringVarP(&output_dir, "output_dir", "o", "", "Output directory for the configuration files.")
 	pullCmd.Flags().StringVarP(&serviceConfig, "service_config", "s", "", "File where the service configurations are describe.")
-	pullCmd.Flags().BoolVar(&verbose, "verbose", false, "Enable Ansible verbosity.")
+	pullCmd.Flags().StringSliceVar(&filters, "filters", []string{}, "Filter Openstack services: --filters glance_api,nova_api,keystone ..")
+	pullCmd.Flags().BoolVar(&update, "update", false, "Update config.yaml with Podman informations.")
+	pullCmd.Flags().BoolVar(&updateOnly, "update-only", false, "Update only config.yaml with Podman informations and not pull configurations from services.")
 	rootCmd.AddCommand(pullCmd)
 }
